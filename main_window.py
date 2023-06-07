df889a40571ceb80658404240cd3c6ba511951c7
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-07 20:53:31 -0300

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -3,7 +3,7 @@ from PySide6.QtWidgets import QMainWindow, QMessageBox, QVBoxLayout, QWidget
 
 class MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
-        super().__init__(parent, *args, **kwargs
+        super().__init__(parent, *args, **kwargs)
 
         # Configurando o layout básico
         self.cw = QWidget
