cf5d51a89237d98890e3b73bbe75faeedf78a9e0
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-08 20:45:08 -0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,7 +2,7 @@
 
 ROOT_DIR = Path(__file__).parent
 FILES_DIR = ROOT_DIR / 'files'
-WINDOW_ICON_PATH = FILES_DIR / icon.png'
+WINDOW_ICON_PATH = FILES_DIR / 'icon.png'
 
 # Colors
 PRIMARY_COLOR = '#1e81b0'
