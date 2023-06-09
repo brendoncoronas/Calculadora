537f7a4d4930c0c6ff21956ae9614ee8249fc16d
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-09 19:10:38 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def convertToNumber(string: str):
 
     # se esse numero poder ser um inteiro
     if Number.is_integer():
-        Number  intNumber
+        Number = intNumber
 
     return Number
 
