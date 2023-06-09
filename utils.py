87f2f386f67132c5bd6bf4b9aed5d80b64408a80
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-09 19:06:20 -0300

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,32 @@
+
+
+# numero ou ponto
+NUM_OR_DOT_REGEX  re.compile(r'^[0-9.]$'
+
+
+def convertToNumber string str
+    Number  float string
+
+    # se esse numero poder ser um inteiro
+    if Number.is_integer
+        Number  intNumber
+
+    return Number
+
+
+def isNumOrDotstring str
+    return boolNUM_OR_DOT_REGEX.search string
+
+
+def isValidNumberstring str
+    valid  False
+    try
+        float string
+        valid  True
+    except ValueError
+        valid  False
+    return valid
+
+
+def isEmptystring: str
+    return lenstring  0
