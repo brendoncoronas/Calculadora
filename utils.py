1230291d9d4dd86b5747febad8c5bb07dcd4f3df
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-09 19:14:19 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def isValidNumber(string: str):
     valid = False
     try:
         float string
-        valid  True
+        valid = True
     except ValueError:
         valid  False
     return valid
