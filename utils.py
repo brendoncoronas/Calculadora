90dc630c2581593f753a4fb906b1a37a32e5c3ad
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-09 19:09:25 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ NUM_OR_DOT_REGEX = re.compile(r'^[0-9.]$')
 
 
 def convertToNumber(string: str):
-    Number  float string
+    Number = float string
 
     # se esse numero poder ser um inteiro
     if Number.is_integer
