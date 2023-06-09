acb3735065d85beba91855da0e0bde6d07e5f181
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-09 19:13:10 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def isNumOrDot(string: str):
     return bool(NUM_OR_DOT_REGEX.search(string))
 
 
-def isValidNumber(string str
+def isValidNumber(string str)
     valid  False
     try
         float string
