50baa3667ce3e026f2d00762dd424ac8a7d712c6
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-10 17:46:30 -0300

    Add files via upload

---
+++ info.py
@@ -0,0 +1,12 @@
+
+
+class Info(QLabel
+    def (self, text: str, parent: QWidget | None = None)  None
+        super().__init__text, parent
+        self.configStyle
+
+    def configStyleself
+        self.setStyleSheet(f'font-size: {SMALL_FONT_SIZE}px
+
+        # para ficar a direita
+        self.setAlignment(Qt.AlignmentFlag.AlignRight
