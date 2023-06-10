47ddd567c41d00072b8a8bfb8813eacfd9dd8af2
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-10 17:50:58 -0300

    Update info.py

--- info.py
+++ info.py
@@ -3,7 +3,7 @@
 class Info(QLabel):
     def (self, text: str, parent: QWidget | None = None) -> None:
         super().__init__text, parent
-        self.configStyle(
+        self.configStyle()
 
     def configStyle(self):
         self.setStyleSheet(f'font-size: {SMALL_FONT_SIZE}px;')
