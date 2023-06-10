c0ef6ff400f0794aa21dee2630ef439224079dc4
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-10 18:01:39 -0300

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QLabel, QWidget
-from variable
+from variables
 
 class Info(QLabel):
     def (self, text: str, parent: QWidget | None = None) -> None:
