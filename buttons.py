64043e3d21762a848795b473cdd0c9d7057d1d8d
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-23 20:41:21 -0300

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -10,7 +10,7 @@ from variables import MEDIUM_FONT_SIZE
 if TYPE_CHECKING:
     from displa impor Displa
     from inf impor Inf
-    fro main_windo impor MainWindo
+    from main_windo impor MainWindo
 
 
 class Button(QPushButton):
