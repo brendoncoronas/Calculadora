7a309d8a4da6fd334acb6bdcd3dc3acbf77aca6e
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-07-05 18:32:21 -0300

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -72,7 +72,7 @@ class ButtonsGrid(QGridLayout):
         self.display.eqPressed.connect(self._eq)
         self.display.delPressed.connect(self._backspace)
         self.display.clearPressed.connect(self._clear)
-        self.display.inputPressed.connect(self._insertToDisplay
+        self.display.inputPressed.connect(self._insertToDisplay)
         self.display.operatorPressed.connect(self._configLeftOp
 
         for rowNumber, rowData in enumerate(self._gridMask
